a947cc0052ef8830d8e797ac6399e10e37426d72
Felix Reidt <freidt@physi.uni-heidelberg.de>
2015-02-25 14:54:36 +0100

    first version

--- generate_results_table_pALPIDEss.sh
+++ generate_results_table_pALPIDEss.sh
@@ -7,7 +7,7 @@ configFile=/data0/alice_u/freidt/ilcsoft/v01-17-05/Eutelescope/trunk/jobsub/exam
 withAlign=1 #1 for aligning each run separately and 0 for using commmon alignment
 whichChip=1 #0 for full scale, 1 for small scale
 runFirst=88
-runLast=88 #4
+runLast=894
 dutID=3
 resultsTable=/data0/alice_u/freidt/pALPIDEss/testbeam/2015/DESY/2015-02-09/pALPIDEss/output/results.csv
 
@@ -19,15 +19,17 @@ for r in $(seq ${runFirst} ${runLast})
 do
     rStr=$(printf "run%06d" $r)
     settingsLine=$(cat $settingsFile | grep "^$r,")
-    efficiencyLine=$(cat ${outputFolder}/${rStr}/analysis.log | grep efficiency )
-    efficiency=$(echo $efficiencyLine | cut -d' ' -f 5)
-    refTracks=$(echo $efficiencyLine | cut -d' ' -f 6)
-    matchedTracks=$(echo $efficiencyLine | cut -d' ' -f 7)
-    testreaderOutput=$($EUDAQ/bin/TestReader.exe -b ${outputFolder}/${rStr}/${rStr}.raw)
     energy=$(echo ${settingsLine} | cut -d',' -f 2)
     dut=$(echo ${settingsLine} | cut -d',' -f 5 | cut -d' ' -f $(($dutID + 1)))
     radLevel=$(echo ${settingsLine} | cut -d',' -f 6 | cut -d' ' -f $(($dutID + 1)))
     rate=$(echo ${settingsLine} | cut -d',' -f 7)
+
+    efficiencyLine=$(cat ${outputFolder}/${rStr}/analysis.log | grep efficiency )
+    efficiency=$(echo $efficiencyLine | cut -d' ' -f 5)
+    refTracks=$(echo $efficiencyLine | cut -d' ' -f 6)
+    matchedTracks=$(echo $efficiencyLine | cut -d' ' -f 7)
+
+    testreaderOutput=$($EUDAQ/bin/TestReader.exe -b ${outputFolder}/${rStr}/${rStr}.raw)
     for subStr in ${testreaderOutput}
     do
         case $subStr in
@@ -57,19 +59,20 @@ do
                 ;;
         esac
     done
-    echo ${energy}
-    echo ${dut}
-    echo ${radLevel}
-    echo ${rate}
-    echo ${efficiency}
-    echo ${refTracks}
-    echo ${matchedTracks}
-    echo ${vbb}
-    echo ${vcasn}
-    echo ${vcasp}
-    echo ${vrst}
-    echo ${vlight}
-    echo ${acqTime}
-    echo ${trigDelay}
-    echo ${config}
+    #echo ${energy}
+    #echo ${dut}
+    #echo ${radLevel}
+    #echo ${rate}
+    #echo ${efficiency}
+    #echo ${refTracks}
+    #echo ${matchedTracks}
+    #echo ${vbb}
+    #echo ${vcasn}
+    #echo ${vcasp}
+    #echo ${vrst}
+    #echo ${vlight}
+    #echo ${acqTime}
+    #echo ${trigDelay}
+    #echo ${config}
+    echo ${r},${energy},${dut},${radLevel},${rate},${vbb},${vcasn},${vcasp},${vrst},${vlight},${trigDelay},${acqTime},1,${efficiency},${refTracks},${matchedTracks},${config} >> ${resultsTable}
 done
