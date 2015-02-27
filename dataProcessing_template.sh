a0a5a1b3a72fd490e4ee02936639d03f52aa7140
Felix Reidt <freidt@physi.uni-heidelberg.de>
2015-02-27 10:44:09 +0100

    removed trailing whitespaces

--- dataProcessing_template.sh
+++ dataProcessing_template.sh
@@ -13,7 +13,7 @@ fi
 
 if ! [ -d $outputFolder ]; then
   mkdir -p $outputFolder
-fi 
+fi
 
 re='^[0-9]+$'
 
@@ -31,7 +31,7 @@ fi
 IFS=$'\n'
 for line in $(cat $settingsFile)
 do
-  if [ "$1" == "ALIGN" ]; then 
+  if [ "$1" == "ALIGN" ]; then
     break
   fi
   IFS=',' read -ra input <<< "$line"
@@ -56,7 +56,7 @@ do
       continue
     fi
     echo -e "\n \n \n REPROCESSING RUN ${input[0]}!!! \n \n \n"
-  elif [ "$1" == "REPROCESS" ]; then 
+  elif [ "$1" == "REPROCESS" ]; then
     if [[ $2 =~ $re ]]; then
       if ! [[ $2 == ${input[0]} ]] ; then
         continue
@@ -89,7 +89,7 @@ do
   git diff   >> `printf $outputFolder/run"%06d"/git_status.txt ${input[0]}`
   git log -1 >> `printf $outputFolder/run"%06d"/git_status.txt ${input[0]}`
   if [ "$1" != "DEBUG" ]; then
-    $CMD_PREFIX ./run_processing.sh ${input[0]} ${DUT[0]} ${DUT[${#DUT[@]}-1]} $settingsFile `printf $outputFolder/run"%06d" ${input[0]}` $rawDataFolder ${#chips[@]} $configFile $whichChip $withAlign $1 & 
+    $CMD_PREFIX ./run_processing.sh ${input[0]} ${DUT[0]} ${DUT[${#DUT[@]}-1]} $settingsFile `printf $outputFolder/run"%06d" ${input[0]}` $rawDataFolder ${#chips[@]} $configFile $whichChip $withAlign $1 &
     fileFound=1
     sleep 5
   elif [ "$#" -eq 2 ]; then
@@ -138,9 +138,9 @@ if [ "$1" == "ALIGN" ]; then
     if ! [[ ${runs[iRun]} =~ $re ]] ; then
       echo "Give as second argument run numbers to be used for aligning "
       exit 1
-    else 
+    else
       found=0
-      for line in $(cat $settingsFile) 
+      for line in $(cat $settingsFile)
       do
         IFS=',' read -ra input <<< "$line"
         if ! [[ ${input[0]} =~ $re ]] ; then
@@ -159,13 +159,13 @@ if [ "$1" == "ALIGN" ]; then
       fi
     fi
   done
-  echo -e "\n \n \n Running in alignment mode \n \n \n" 
+  echo -e "\n \n \n Running in alignment mode \n \n \n"
   outputAlign=$outputFolder/align/
   outputFolder=$outputAlign
   echo $outputFolder
   if ! [ -d $outputFolder ]; then
     mkdir $outputFolder
-  fi 
+  fi
   mkdir `printf $outputFolder/run"%06d"-"%06d" $3 $4` `printf $outputFolder/run"%06d"-"%06d"/histogram $3 $4` `printf $outputFolder/run"%06d"-"%06d"/lcio $3 $4` `printf $outputFolder/run"%06d"-"%06d"/database $3 $4` `printf $outputFolder/run"%06d"-"%06d"/logs $3 $4`
   cd $EUTELESCOPE
   echo "EUTELESCOPE: " > `printf $outputFolder/run"%06d"-"%06d"/git_status.txt $3 $4`
