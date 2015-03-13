27ec23456d7b870d659b99ef6aca8b527ea963a3
Felix Reidt <freidt@physi.uni-heidelberg.de>
2015-03-13 08:32:49 +0100

    taking the maximum of the timestamp histogram from the pALPIDEfs, not the pALPIDEss

--- run_processing.sh
+++ run_processing.sh
@@ -295,7 +295,7 @@ elif (( ( ${13}==0 && $place <= 100) || ${13}==1)); then
     exit 0
   fi
   minTimeStamp=0
-  minTimeStamp=`cat $clusteringLogName |  sed -n -e "s/^.*Maximum of the time stamp histo is at //p" | bc -l`
+  minTimeStamp=`cat $clusteringLogName | grep pALPIDEfs | sed -n -e "s/^.*Maximum of the time stamp histo is at //p" | bc -l`
   if [[ ${9} -eq 1 ]] && [[ $minTimeStamp -lt 12000 ]]; then
       echo "Minimum timestamp was too low ("$minTimeStamp") overidding it with 12000"
       minTimeStamp=12000
