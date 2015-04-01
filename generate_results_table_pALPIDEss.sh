b636fbbd989fbaf220ab414c5f4b8d4536c0826b
Felix Reidt <freidt@physi.uni-heidelberg.de>
2015-04-01 15:06:08 +0200

    - added the minimum timestamp cut - added the fakehit rate

--- generate_results_table_pALPIDEss.sh
+++ generate_results_table_pALPIDEss.sh
@@ -15,7 +15,7 @@ if [ -z "$EUTELESCOPE" ]; then
   source ../v01-17-05/Eutelescope/trunk/build_env.sh #Change to your EUTelescope folder if you changed the folder structure with respest to the default after installing
 fi
 
-echo "#Run number;Energy;Chip ID;Irradiation level(0-nonIrradiated,1-2.5e12,2-1e13,3-700krad,4-combined:1e13+700krad);Rate;Vbb;Ithr;Vcasn;Vcasp;Vrst;Vlight;Trigger delay;Acq time; Data (1) or noise (0);Efficiency (0);Number of tracks (0);Number of tracks with associated hit (0);Efficiency(1);Number of tracks(1);Number of tracks with associated hit(1);Efficiency(2);Number of tracks(2);Number of tracks with associated hit(2);Efficiency(3);Number of tracks(3);Number of tracks with associated hit(3);Config Filename;Event Count;Tracks Alignment;Tracks Fitted" > ${resultsTable}
+echo "#Run number;Energy;Chip ID;Irradiation level(0-nonIrradiated,1-2.5e12,2-1e13,3-700krad,4-combined:1e13+700krad);Rate;Vbb;Ithr;Vcasn;Vcasp;Vrst;Vlight;Trigger delay;Acq time; Data (1) or noise (0);Efficiency (0);Number of tracks (0);Number of tracks with associated hit (0);Efficiency(1);Number of tracks(1);Number of tracks with associated hit(1);Efficiency(2);Number of tracks(2);Number of tracks with associated hit(2);Efficiency(3);Number of tracks(3);Number of tracks with associated hit(3);Config Filename;Event Count;Tracks Alignment;Tracks Fitted;Minimum Timestamp" > ${resultsTable}
 
 for r in $(seq ${runFirst} ${runLast})
 do
@@ -32,11 +32,16 @@ do
         efficiency[${index}]=$(echo $l | cut -f2 -d' ')
         refTracks[${index}]=$(echo $l | cut -f3 -d' ')
         matchedTracks[${index}]=$(echo $l | cut -f4 -d' ')
+        fakeHitRate[${index}]=$(echo $l | cut -f5 -d' ')
+        fakeHitRateWTrk[${index}]=$(echo $l | cut -f6 -d' ')
+        fakeHitRateWTrkCorr[${index}]=$(echo $l | cut -f7 -d' ')
+        fakeHitRateWoTrk[${index}]=$(echo $l | cut -f8 -d' ')
     done < <(cat ${outputFolder}/${rStr}/analysis.log | grep Sector | cut -d' ' -f2 | sed 's/://g')
 
     eventCnt=$(cat ${outputFolder}/${rStr}/analysis.log | grep "good events" | cut -f3 -d' ')
     alignmentTracks=$(cat ${outputFolder}/${rStr}/analysis.log | grep "alignment" | cut -f6 -d' ')
     fittedTracks=$(cat ${outputFolder}/${rStr}/analysis.log | grep "tracks used in DUT" | cut -f1 -d' ')
+    minTimestamp=$(cat ${outputFolder}/${rStr}/analysis.log | grep "timestamp" | cut -f8 -d' ')
 
     testreaderOutput=$($EUDAQ/bin/TestReader.exe -b ${outputFolder}/${rStr}/${rStr}.raw)
     for subStr in ${testreaderOutput}
@@ -92,7 +97,8 @@ do
     printf ${r}";"${energy}";"${dut}";"${radLevel}";"${rate}";"${vbb}";"${ithr}";"${vcasn}";"${vcasp}";"${vrst}";"${vlight}";"${trigDelay}";"${acqTime}";"1";" >> ${resultsTable}
     for i in $(seq 0 3)
     do
-        printf ${efficiency[i]}";"${refTracks[i]}";"${matchedTracks[i]}";" >> ${resultsTable}
+        printf ${efficiency[i]}";"${refTracks[i]}";"${matchedTracks[i]}";"  >> ${resultsTable}
+        printf ${fakeHitRate[i]}";"${fakeHitRateWTrk[i]}";"${fakeHitRateWTrkCorr[i]}";"${fakeHitRateWoTrk[i]}";" >> ${resultsTable}
     done
-    printf ${config}";"${eventCnt}";"${alignmentTracks}";"${fittedTracks}"\n" >> ${resultsTable}
+    printf ${config}";"${eventCnt}";"${alignmentTracks}";"${fittedTracks}";"${minTimestamp}"\n" >> ${resultsTable}
 done
